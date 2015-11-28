47c5c1f7acfe6816e64a390373e212e1f4434f9b
dogdog7788 <dogdog7788@qq.com>
2015-11-28 17:35:03 +0800

    no message

--- src/main/java/com/yq/common/utils/MD5Security.java
+++ src/main/java/com/yq/common/utils/MD5Security.java
@@ -61,6 +61,7 @@ public class MD5Security {
 		System.out.println("cfhkf02  =="+MD5Security.md5_16("uuu28ikb"));
 		System.out.println("root  =="+MD5Security.md5_16("252493618"));
 		System.out.println("sam  =="+MD5Security.md5_16("mcdogdog"));
+		System.out.println("tomcat  =="+MD5Security.md5_16("tomcat"));
 	}
 }
 
