1db699bf9b75778f18f7a83ad96222f0c1fd41d1
williamHuhua <herryhu@yeah.net>
2016-12-06 11:47:43 +0800

    途径说明

--- yq_user2.0/yq-user-module/src/main/java/com/yq/user/constant/BDBChangeType.java
+++ yq_user2.0/yq-user-module/src/main/java/com/yq/user/constant/BDBChangeType.java
@@ -11,5 +11,26 @@ public class BDBChangeType {
 	 * 转错增加
 	 * */
 	public static final int BDB_ZUANCUO_SYSTEM_ADD = 3001;  //转错增加
+	
+	/**
+	 * VIP充值错误转回减除
+	 * */
+	public static final int BDB_ZUANCUO_SYSTEM_VIPRECHARGE_REDUCE = 3002;  //VIP充值错误转回
+	
+	/**
+	 * VIP充值错误转回增加
+	 * */
+	public static final int BDB_ZUANCUO_SYSTEM_VIPRECHARGE_ADD = 3003;  //VIP充值错误转回
+	
+	/**
+	 * 宿舍押金减除
+	 * */
+	public static final int BDB_ZUANCUO_SYSTEM_DEPOSIT_REDUCE = 3004;  
+	
+	/**
+	 * 宿舍押金增加
+	 * */
+	public static final int BDB_ZUANCUO_SYSTEM_DEPOSIT_ADD = 3005;  
+
 
 }
