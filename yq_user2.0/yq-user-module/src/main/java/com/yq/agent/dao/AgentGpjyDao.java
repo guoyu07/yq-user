bf6082ba889bdc81e909c1b0e61cf036f7b9714a
williamHuhua <herryhu@yeah.net>
2017-06-05 10:41:33 +0800

    积分详情只显示已成交部分。

--- yq_user2.0/yq-user-module/src/main/java/com/yq/agent/dao/AgentGpjyDao.java
+++ yq_user2.0/yq-user-module/src/main/java/com/yq/agent/dao/AgentGpjyDao.java
@@ -10,7 +10,7 @@ public class AgentGpjyDao extends YqDaoBase<Gpjy>{
 	
 	
 		public IPage<Gpjy> getAllGpjyPageList(String userName, int pageIndex, int pageSize) {
-			String sql="select * from gpjy where username = ? and bz not like '拆分%倍' order by id desc";
+			String sql="select * from gpjy where username = ? and jy = 1 and  bz not like '拆分%倍'  order by id desc";
 			return getJdbc().getListPage(sql, Gpjy.class, SqlParameter.Instance().withString(userName), pageSize, pageIndex);
 		}
 	
